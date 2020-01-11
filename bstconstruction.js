030bf693cfdb69443a8f31edaff2aceaee022809
utkarsh <utkarsh.bharti30@gmail.com>
2020-01-11 14:57:55 +0530

    added time and space complexity

--- bstconstruction.js
+++ bstconstruction.js
@@ -5,10 +5,16 @@ class node {
     this.right = null;
   }
 }
+
+// time complexity  : avaerage time - O(log (n))
+//                    worst time - O(n), in case of linear bst
+// space complexity : O(1) // if implemented iteratively
+//                 : O(n) // if implemented recursively , due to call stack
 class bst {
   constructor() {
     this.root = null;
   }
+  // this is an iterative approach
   insert(value) {
     const newNode = new node(value);
     let current = this.root;
@@ -35,6 +41,20 @@ class bst {
     }
     return this;
   }
+  findNode(value) {
+    if (this.root === null) return null;
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return current;
+      }
+    }
+    return null;
+  }
 }
 
 const bstree = new bst();
@@ -44,4 +64,5 @@ bstree.insert(15);
 bstree.insert(3);
 bstree.insert(8);
 bstree.insert(20);
-console.log(bstree);
+
+console.log(bstree.findNode(200));
