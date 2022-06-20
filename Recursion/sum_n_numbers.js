5e8aef62aa9288d65759bed8edb2d6618390c584
utkarsh <naruto@Narutos-MacBook-Air.local>
2022-06-20 14:10:59 +0530

    sum of numbers upto n

---
+++ Recursion/sum_n_numbers.js
@@ -0,0 +1,22 @@
+const ip = 10,
+  op = 55;
+
+// parameterised function call
+const sumType1 = (num, currentSum) => {
+  if (num <= 0) {
+    return currentSum;
+  }
+  return sumType1(num - 1, num + currentSum);
+};
+
+console.log(sumType1(ip, 0)); // true
+
+// Functional recursion
+const sumType2 = (num) => {
+  if (num === 0) {
+    return 0;
+  }
+  return num + sumType2(num - 1);
+};
+
+console.log(sumType2(ip)); // true
